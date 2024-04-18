01312eb55b1e334d65fab3d943ce30c96289b94d
Grigoriy Avirom <88avirom@gmail.com>
2024-04-18 23:37:05 +0300

    Pywinauto: запуск и останов тестируемого приложения

---
+++ conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from fixture.application import Application
+
+@pytest.fixture(scope="session")
+def app(request):
+    fixture = Application("C:\\StudySoftwareTesting\\FreeAddressBookPortable\\AddressBook.exe")
+    request.addfinalizer(fixture.destroy)
+    return fixture
