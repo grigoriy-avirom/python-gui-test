f5a54ff7196074a09dedab5c50f83056db214dc6
Grigoriy Avirom <88avirom@gmail.com>
2024-04-19 11:05:36 +0300

    Pywinauto: взаимодействие с элементами управления

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,5 @@
 from pywinauto.application import Application as WinApplication
+from fixture.group import GroupHelper
 
 
 class Application:
@@ -6,5 +7,6 @@ class Application:
         self.application = WinApplication(backend="win32").start(target) #uia
         self.main_window = self.application.window(title="Free Address Book")
         self.main_window.wait("visible")
+        self.groups = GroupHelper(self)
     def destroy(self):
         self.main_window.close()
